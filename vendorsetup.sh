4b337c5bb8e5d42b61e984d5b111669805a510a8
Introdructor <yashkachave7@gmail.com>
2021-01-12 10:10:10 +0530

    ginkgo:update vendorsetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -22,8 +22,8 @@ git clone https://github.com/LineageOS/android_vendor_nxp_opensource_packages_ap
 git clone https://github.com/LineageOS/android_vendor_nxp_opensource_halimpl.git -b lineage-18.1-sn100x vendor/nxp/opensource/sn100x/halimpl
 git clone https://github.com/LineageOS/android_vendor_nxp_opensource_hidlimpl.git -b lineage-18.1-sn100x vendor/nxp/opensource/sn100x/hidlimpl
 git clone https://github.com/LineageOS/android_external_libnfc-nxp external/libnfc-nxp
-git clone https://github.com/ghostrider-reborn/android_hardware_qcom_display.git -b arrow-11.0-caf-sm6125-R hardware/qcom-caf/sm8150/display
-git clone https://github.com/ArrowOS/android_hardware_qcom_audio.git -b  arrow-11.0-caf-sm8150 hardware/qcom-caf/sm8150/audio
+git clone https://github.com/ArrowOS/android_hardware_qcom_display.git -b arrow-11.0-caf-sm8150 hardware/qcom-caf/sm8150/display
+git clone https://github.com/ArrowOS/android_hardware_qcom_audio.git -b arrow-11.0-caf-sm8150 hardware/qcom-caf/sm8150/audio
 git clone https://github.com/ArrowOS/android_hardware_qcom_media.git -b arrow-11.0-caf-sm8150 hardware/qcom-caf/sm8150/media
 git clone https://github.com/ArrowOS/android_vendor_qcom_opensource_power.git vendor/qcom/opensource/power
 git clone https://github.com/ArrowOS/android_vendor_qcom_opensource_interfaces.git vendor/qcom/opensource/interfaces
