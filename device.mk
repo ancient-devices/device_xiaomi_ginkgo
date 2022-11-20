81dfd1b83afed002751e8219245beaeaff0bcd20
Erwin Abu Bakar Sidik <64487987+erwinabs@users.noreply.github.com>
2022-11-21 04:34:46 +0700

    ginkgo: build fingerprint

--- device.mk
+++ device.mk
@@ -154,6 +154,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.3.vendor \
     android.hardware.biometrics.fingerprint@2.3-service.ginkgo
 
 PRODUCT_COPY_FILES += \
